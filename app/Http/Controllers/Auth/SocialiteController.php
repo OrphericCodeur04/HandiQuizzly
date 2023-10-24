f1ad3db27d0a2984c22a55e19a8c0a4b37c43480
Orpheric ALLAGBE <florentallgbe229@gmail.com>
2023-10-24 19:05:12 +0100

    define socialite controller

---
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SocialiteController extends Controller
+{
+    public function loginSocial(Request $request, string $provider): RedirectResponse
+    {
+        $this->validateProvider($request);
+ 
+        return Socialite::driver($provider)->redirect();
+    }
+ 
+    public function callbackSocial(Request $request, string $provider)
+    {
+        $this->validateProvider($request);
+ 
+        $response = Socialite::driver($provider)->user();
+ 
+        $user = User::firstOrCreate(
+            ['email' => $response->getEmail()],
+            ['password' => Str::password()]
+        );
+        $data = [$provider . '_id' => $response->getId()];
+ 
+        if ($user->wasRecentlyCreated) {
+            $data['name'] = $response->getName() ?? $response->getNickname();
+ 
+            event(new Registered($user));
+        }
+ 
+        $user->update($data);
+ 
+        Auth::login($user, remember: true);
+ 
+        return redirect()->intended(RouteServiceProvider::HOME);
+    }
+ 
+    protected function validateProvider(Request $request): array
+    {
+        return $this->getValidationFactory()->make(
+            $request->route()->parameters(),
+            ['provider' => 'in:facebook,google,github']
+        )->validate();
+    }
+}
