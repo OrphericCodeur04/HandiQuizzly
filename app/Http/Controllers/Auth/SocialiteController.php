5d8a163062b644783aaa336a6f492af741d73dd8
Orpheric ALLAGBE <florentallgbe229@gmail.com>
2023-10-24 21:42:00 +0100

    initialize socialite package in Controller

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
