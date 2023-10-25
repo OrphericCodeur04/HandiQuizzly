7f62c39af931ceb0c3d96a1f9cb2512c23be4c3d
Orpheric ALLAGBE <florentallgbe229@gmail.com>
2023-10-25 17:12:34 +0100

    add QuestionList livewire component, routes web

---
+++ app/Livewire/Questions/QuestionList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Questions;
+
+use Livewire\Component;
+
+class QuestionList extends Component
+{
+    public function render()
+    {
+        return view('livewire.questions.question-list');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::middleware('is_admin')->group(function () {
+        Route::get('questions', QuestionList::class)->name('questions');
+    });
+
 });
 
 require __DIR__.'/auth.php';
